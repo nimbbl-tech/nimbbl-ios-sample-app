3583fb1b0324a7c2045a30392ff1aefe47ab7da0
Stany Dsouza <stany.dsouza@mswipe.in>
2021-04-30 19:57:31 +0530

    first push

---
+++ PaymentApp/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  PaymentApp
+//
+//  Created by Stany on 12/03/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+
---
+++ Pods/Nimbbl-SDK/README.md
@@ -0,0 +1,1 @@
+Nimbbl ios Pod
