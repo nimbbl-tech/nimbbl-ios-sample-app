1eeb552a918e686072dab692b7aaf36427c20142
stanydsouzall <stany@logicloop.io>
2021-08-04 18:11:40 +0530

    sdk integrated as framework

---
+++ PaymentApp/NimbblCheckoutSDK.framework/Headers/NimbblCheckoutSDK.h
@@ -0,0 +1,18 @@
+//
+//  NimbblCheckoutSDK.h
+//  NimbblCheckoutSDK
+//
+//  Created by Stany on 09/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NimbblCheckoutSDK.
+FOUNDATION_EXPORT double NimbblCheckoutSDKVersionNumber;
+
+//! Project version string for NimbblCheckoutSDK.
+FOUNDATION_EXPORT const unsigned char NimbblCheckoutSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NimbblCheckoutSDK/PublicHeader.h>
+
+
